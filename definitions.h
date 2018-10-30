75ab2ef3292fd95e1c1c42ac1a0715498a341d59
Ihor Berizka <ihor.berizka@gmail.com>
2018-10-30 22:39:01 +0200

    Moved hardcoded keys of wifi configs to definitions.h

--- definitions.h
+++ definitions.h
@@ -16,5 +16,6 @@
 #define INVALID_WIFI_CONFIG_RESPONSE_HEADER 1u
 #define WIFI_CONNECTION_FAILED_RESPONSE_HEADER 2u
 
-
+#define WIFI_CONFIG_SSID_KEY ("wifi_ssid")
+#define WIFI_CONFIG_PWD_KEY ("wifi_pwd")
 #endif
