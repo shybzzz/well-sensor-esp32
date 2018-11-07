bddaedd4604a9c31691f0e35b2cd48fec34e1b35
Ihor Berizka <ihor.berizka@gmail.com>
2018-11-07 22:57:01 +0200

    Added new member to struct.
    This member will be used in mqtt state.

--- init-qr.h
+++ init-qr.h
@@ -2,15 +2,20 @@
 #define __INIT_QR__
 
 struct QRConfig {
-  char AP_SSID[64];
-  char AP_PWD[64];
+  char AP_SSID[MAX_STR_LEN];
+  char AP_PWD[MAX_STR_LEN];
+  char DEVICE_ID[MAX_STR_LEN];
 };
 
 QRConfig qrConfig;
 
 void initQr() {
-  strncpy(qrConfig.AP_SSID, "Well Sensor", sizeof(qrConfig.AP_SSID));
-  strncpy(qrConfig.AP_PWD, "11111111", sizeof(qrConfig.AP_PWD));    
+  char ssid[] = "Well Sensor";
+  char pwd[] = "11111111";
+  char device_id[] = "ESP_Client_Ihor";
+  strncpy(qrConfig.AP_SSID, ssid, strlen(ssid));
+  strncpy(qrConfig.AP_PWD, pwd, strlen(pwd)); 
+  strncpy(qrConfig.DEVICE_ID, device_id, strlen(device_id));  
 }
 
 #endif
