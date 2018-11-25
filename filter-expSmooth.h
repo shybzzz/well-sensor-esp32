5b2ad28d4c0785623c9dfea2b2c7e593facc6260
Ihor Berizka <ihor.berizka@gmail.com>
2018-11-25 21:11:24 +0200

    Added expSmooth filter

---
+++ filter-expSmooth.h
@@ -0,0 +1,20 @@
+#ifndef __FILTER_EXP_SMOOTH__
+#define __FILTER_EXP_SMOOTH__
+
+int filterExpSmooth()
+{
+  double alp = 0.5;
+  static int prev_filtered_sample = -1;
+  if (prev_filtered_sample < 0)
+  {
+    prev_filtered_sample = data[current_sample-1];  
+  }  
+  else
+  {
+    int new_filtered_sample = data[current_sample-1] * alp + (1.0 - alp) * prev_filtered_sample;
+    prev_filtered_sample = new_filtered_sample;
+  }
+  return prev_filtered_sample;
+} 
+#endif
+
--- init-data.h
+++ init-data.h
@@ -3,8 +3,9 @@
 
 
 #define DATA_SIZE 9u
-#define DATA_SIZE_H DATA_SIZE/2
-
+#define MEDIAN_WINDOW 3u
+#define MEDIAN_MID MEDIAN_WINDOW/2
+#define EXP_SMOOTH_WINDOW 1u
 int data[DATA_SIZE];
 uint8_t current_sample = 0;
 
