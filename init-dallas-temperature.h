39c01dcdde5bb3504d635730634d31f068dd1294
shybzzz <rostykrom@yahoo.com>
2018-12-28 06:36:26 +0200

    comment

--- init-dallas-temperature.h
+++ init-dallas-temperature.h
@@ -10,9 +10,11 @@
 
 #define DALLAS_TEMPERATURE_SENSOR_RESOLUTION 12u
 
+#define MAX_DALLAS_TEMPERATURE_SENSORS_COUNT 2u
+
 OneWire ds(DALLAS_TEMPERATURE_PIN);
 DallasTemperature dallasSensors(&ds);
-DeviceAddress* sensorAddress;
+DeviceAddress sensorAddress[MAX_DALLAS_TEMPERATURE_SENSORS_COUNT];
 uint8_t connectedSensors = 0;
 
 void printAddress(DeviceAddress deviceAddress) {
@@ -28,7 +30,6 @@ void initDallasSensor()
 {
   dallasSensors.begin();
   connectedSensors = dallasSensors.getDeviceCount();
-  sensorAddress = new DeviceAddress[connectedSensors];
 
   Serial.print("Search sensors...");
   Serial.print("Found: ");
@@ -37,7 +38,7 @@ void initDallasSensor()
 
   // find sensor:
   // find sensor addr by index.
-  for (uint8_t i = 0; i < connectedSensors; ++i)
+  for (uint8_t i = 0; i < connectedSensors || i < MAX_DALLAS_TEMPERATURE_SENSORS_COUNT; ++i)
   {
     if (dallasSensors.getAddress(sensorAddress[i], i)) {
       dallasSensors.setResolution(sensorAddress[i], DALLAS_TEMPERATURE_SENSOR_RESOLUTION);
